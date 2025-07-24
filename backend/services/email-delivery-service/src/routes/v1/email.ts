fd81ba476405b8f19bf39a81c5eeb7efb9ac82a8
Nirmal Basnet <nirmalbasnet@192.168.101.8>
2025-07-25 01:36:34 +0545

    feat: nginx as reverse proxy

--- backend/services/email-delivery-service/src/routes/v1/email.ts
+++ backend/services/email-delivery-service/src/routes/v1/email.ts
@@ -4,6 +4,10 @@ import EmailSchema from "schema/EmailSchema";
 import * as sendEmailController from "controllers/sendEmailController";
 
 const routes = async (fastify: FastifyInstance) => {
+  fastify.get("/", async (req: FastifyRequest, res: FastifyReply) => {
+    res.type("text/html").send("<h1>Welcome to the notification service</h1>");
+  });
+
   fastify.get("/ping", async (req: FastifyRequest, res: FastifyReply) => {
     res.type("text/html").send("<h1>Welcome to the notification service</h1>");
   });
